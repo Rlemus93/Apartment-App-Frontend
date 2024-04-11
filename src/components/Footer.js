27545be435a71496d406fc3edad580a0a44f86eb
sethcrist <sethcrist50@gmail.com>
2024-04-11 14:28:22 -0700

    added a header and footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <p className="footer-text">
+      <span>
+        &copy; 2024{" "}
+        <a
+          className="github-links"
+          href="https://github.com/Rlemus93"
+          target="_blank"
+        >
+          Ryan Lemus
+        </a>{" "}
+        |{" "}
+        <a
+          className="github-links"
+          href="https://github.com/sethcrist"
+          target="_blank"
+        >
+          Seth Crist
+        </a>{" "}
+      </span>
+    </p>
+  )
+}
+
+export default Footer
